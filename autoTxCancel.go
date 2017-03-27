68ee6a5aa04ca2506f12dfa1e79259b365badf06
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-03-26 20:23:21 -0700

    merge

--- autoTxCancel.go
+++ autoTxCancel.go
@@ -12,22 +12,23 @@ import (
 type AutoTxCancel struct {
 	Stock    string
 	UserID   string
-	WorkerID uint64
+	Action   string
+	WorkerID int
 }
 
 // ToCSV : Converts the QuoteRequest to a csv
 func (aTx *AutoTxCancel) ToCSV() string {
-	return fmt.Sprintf("%s,%s,%d", aTx.Stock, aTx.UserID, aTx.WorkerID)
+	return fmt.Sprintf("%s,%s,%s,%d", aTx.Stock, aTx.UserID, aTx.Action, aTx.WorkerID)
 }
 
 // ParseAutoTxCancel : Parses CSV as QuoteRequest
 func ParseAutoTxCancel(csv string) (AutoTxCancel, error) {
 	parts := strings.Split(csv, ",")
-	if len(parts) != 3 {
+	if len(parts) != 4 {
 		return AutoTxCancel{}, errors.New("Expected number of values in AutoTxCancel csv")
 	}
 
-	workerNum, err := strconv.ParseUint(parts[2], 10, 64)
+	workerNum, err := strconv.ParseUint(parts[3], 10, 64)
 	if err != nil {
 		return AutoTxCancel{}, err
 	}
@@ -35,6 +36,7 @@ func ParseAutoTxCancel(csv string) (AutoTxCancel, error) {
 	return AutoTxCancel{
 		Stock:    parts[0],
 		UserID:   parts[1],
-		WorkerID: workerNum,
+		Action:   parts[2],
+		WorkerID: int(workerNum),
 	}, nil
 }
--- autoTxInit.go
+++ autoTxInit.go
@@ -8,34 +8,47 @@ import (
 	"strconv"
 
 	currency "github.com/distributeddesigns/currency"
+	"github.com/petar/GoLLRB/llrb"
 )
 
 // AutoTxInit : Request for auto transaction init
 type AutoTxInit struct {
 	Amount   currency.Currency
 	Trigger  currency.Currency
+	Action   string
 	Stock    string
 	UserID   string
-	WorkerID uint64
+	WorkerID int
 }
 
 // ToCSV : Converts the QuoteRequest to a csv
 func (aTx *AutoTxInit) ToCSV() string {
-	parts := make([]string, 5)
+	parts := make([]string, 6)
 
 	parts[0] = fmt.Sprintf("%0.2f", aTx.Amount.ToFloat())
 	parts[1] = fmt.Sprintf("%0.2f", aTx.Trigger.ToFloat())
-	parts[2] = aTx.Stock
-	parts[3] = aTx.UserID
-	parts[4] = strconv.FormatUint(aTx.WorkerID, 10)
+	parts[2] = aTx.Action
+	parts[3] = aTx.Stock
+	parts[4] = aTx.UserID
+	parts[5] = strconv.FormatUint(aTx.WorkerID, 10)
 
 	return strings.Join(parts, ",")
+}
+
+// Less : Interface function for llrb
+func (aTx AutoTxInit) less(than AutoTxInit) bool {
+	return (aTx.Trigger.ToFloat() < than.Trigger.ToFloat())
+}
+
+// Less : Interface function for llrb
+func (aTx AutoTxInit) Less(than llrb.Item) bool {
+	return aTx.less(than.(AutoTxInit))
 }
 
 // ParseAutoTxInit : Parses CSV as QuoteRequest
 func ParseAutoTxInit(csv string) (AutoTxInit, error) {
 	parts := strings.Split(csv, ",")
-	if len(parts) != 5 {
+	if len(parts) != 6 {
 		return AutoTxInit{}, errors.New("Expected number of values in AutoTxInit csv")
 	}
 
@@ -47,7 +60,7 @@ func ParseAutoTxInit(csv string) (AutoTxInit, error) {
 	if err != nil {
 		return AutoTxInit{}, err
 	}
-	workerNum, err := strconv.ParseUint(parts[4], 10, 64)
+	workerNum, err := strconv.ParseInt(parts[5], 10, 64)
 	if err != nil {
 		return AutoTxInit{}, err
 	}
@@ -55,8 +68,9 @@ func ParseAutoTxInit(csv string) (AutoTxInit, error) {
 	return AutoTxInit{
 		Amount:   currAmount,
 		Trigger:  currTrigger,
-		Stock:    parts[2],
-		UserID:   parts[3],
-		WorkerID: workerNum,
+		Action:   parts[2],
+		Stock:    parts[3],
+		UserID:   parts[4],
+		WorkerID: int(workerNum),
 	}, nil
 }
